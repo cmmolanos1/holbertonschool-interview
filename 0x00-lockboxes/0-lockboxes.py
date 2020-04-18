bcdfda25f2c9a326c0d811f1c9f5838e7727da56
cmmolanos1 <926@holbertonschool.com>
2020-04-18 18:22:43 -0500

    [Update]: Adding Documentation.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -53,16 +53,25 @@
 
 # ------------------------------ITERATIVE WAY --------------------------------
 def canUnlockAll(boxes):
-    opened_boxes = [0]
-    n_boxes = [n for n in range(len(boxes))]
+    """Says if all boxes can be opened by opening first the box #0.
 
+    Args:
+        boxes (list): list of lists with information of all boxes/keys.
+
+    Returns:
+        bool: True if all boxes could be opened. Otherwise False.
+    """
+    opened_boxes = [0]
     i = 0
+
     while i < len(opened_boxes):
+        # Pass if empty or all keys of opened boxes
         if boxes[opened_boxes[i]] == [] or \
                 (set(boxes[opened_boxes[i]]).issubset(opened_boxes)):
             pass
         else:
             for b in boxes[opened_boxes[i]]:
+                # Append the box if not wrong number or opened before
                 if b < len(boxes) and b not in opened_boxes:
                     opened_boxes.append(b)
                 else:
