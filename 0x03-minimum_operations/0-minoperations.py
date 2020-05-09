c7a5a7660b4bb8336b4a7cc41be234d7a5632b07
Carlos Molano <926@holbertonschool.com>
2020-05-09 11:55:07 -0500

    [Update] add documentation.

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,10 +3,18 @@
 
 
 def minOperations(n):
+    """Calculares the minimum number of copyall/paste operations.
+
+    Args:
+        n: goal number
+
+    Returns:
+        int: number of operations.
+    """
     number = 1
     copy = 0
     operations = 0
-    if n < 2:
+    if type(n) is not int or n < 2:
         return 0
     else:
         for i in range(n):
