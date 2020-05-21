721bec4650319c65a87cf07929c76ace9e0269ef
Carlos <926@holbertonschool.com>
2020-05-20 21:20:08 -0500

    Upload

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+* palindrome - check if a linked list is palindrome.
+*
+* @head: Pointer to the parent node.
+* @n: Number of jumps until last node.
+* Return: 1 if palindrome, otherwise 0.
+*/
+int palindrome(listint_t *head, int n)
+{
+	listint_t *left, *right;
+	int i;
+
+	if (head == NULL)
+		return (1);
+
+	left = head;
+	right = left;
+
+
+	for (i = 0; i < n; i++)
+		right = right->next;
+
+	if (head == NULL)
+		return (1);
+	else if ((n == 0 || n == 1) && left->n == right->n)
+		return (1);
+	else if (left->n == right->n)
+		return (palindrome(left->next, n - 2));
+	else
+		return (0);
+}
+/**
+* is_palindrome - check if a linked list is palindrome.
+*
+* @head: Pointer to the parent node.
+* Return: 1 if palindrome, otherwise 0.
+*/
+int is_palindrome(listint_t **head)
+{
+	int n = 0;
+	listint_t *aux;
+
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	aux = *head;
+
+	while (aux->next != NULL)
+	{
+		n++;
+		aux = aux->next;
+	}
+
+	return (palindrome(*head, n));
+}
