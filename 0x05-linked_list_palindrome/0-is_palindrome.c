0f0b3f8ab1f7f6f312a3d8f7dce6a1bdfb6d4eed
Carlos <926@holbertonschool.com>
2020-05-24 01:28:52 -0500

    Add the version with worse efficiency

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -3,12 +3,12 @@
 * palindrome - check if a linked list is palindrome.
 *
 * @left: Pointer to the parent node.
-* @right: auxuliar pointer
+* @right: Pointer to the tail node of the list.
 * Return: 1 if palindrome, otherwise 0.
 */
 int palindrome(listint_t **left, listint_t *right)
 {
-	int sublist;
+	int is_pal;
 
 	if (right == NULL)
 		return (1);
@@ -16,10 +16,10 @@ int palindrome(listint_t **left, listint_t *right)
 	if (palindrome(left, right->next) == 0)
 		return (0);
 
-	sublist = (right->n == (*left)->n);
+	is_pal = (right->n == (*left)->n);
 
 	*left = (*left)->next;
-	return (sublist);
+	return (is_pal);
 }
 /**
 * is_palindrome - check if a linked list is palindrome.
@@ -34,3 +34,51 @@ int is_palindrome(listint_t **head)
 
 	return (palindrome(head, *head));
 }
+
+
+/*
+* --------------PREVIOUS VERSION------------
+* int palindrome(listint_t *head, int n)
+*{
+*	listint_t *left, *right;
+*	int i;
+*
+*	if (head == NULL)
+*		return (1);
+*
+*	left = head;
+*	right = left;
+*
+*
+*	for (i = 0; i < n; i++)
+*		right = right->next;
+*
+*	if (head == NULL)
+*		return (1);
+*	else if ((n == 0 || n == 1) && left->n == right->n)
+*		return (1);
+*	else if (left->n == right->n)
+*		return (palindrome(left->next, n - 2));
+*	else
+*		return (0);
+*}
+*
+*int is_palindrome(listint_t **head)
+*{
+*	int n = 0;
+*	listint_t *aux;
+*
+*	if (head == NULL || *head == NULL)
+*		return (1);
+*
+*	aux = *head;
+*
+*	while (aux->next != NULL)
+*	{
+*		n++;
+*		aux = aux->next;
+*	}
+*
+*	return (palindrome(*head, n));
+*}
+*/
