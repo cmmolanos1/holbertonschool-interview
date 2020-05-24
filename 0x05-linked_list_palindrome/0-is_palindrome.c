d1c34c5f4a62c55e2beea60249e093a3e6fc61d6
Carlos <926@holbertonschool.com>
2020-05-24 00:30:13 -0500

    Delete for

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -4,28 +4,28 @@
 * palindrome - check if a linked list is palindrome.
 *
 * @head: Pointer to the parent node.
-* @n: Number of jumps until last node.
+* @tail: Number of jumps until last node.
 * Return: 1 if palindrome, otherwise 0.
 */
-int palindrome(listint_t *head, int n)
+int palindrome(listint_t *head, listint_t *tail)
 {
-	listint_t *left, *right;
-	int i;
+	listint_t *left, *right, *aux;
 
 	if (head == NULL)
 		return (1);
 
 	left = head;
-	right = left;
+	right = tail;
+	aux = left;
 
-
-	for (i = 0; i < n; i++)
-		right = right->next;
-
-	if ((n == 0 || n == 1) && left->n == right->n)
+	if ((left == right || left->next == right) && left->n == right->n)
 		return (1);
 	else if (left->n == right->n)
-		return (palindrome(left->next, n - 2));
+	{
+		while (aux->next != right)
+			aux = aux->next;
+		return (palindrome(left->next, aux));
+	}
 	else
 		return (0);
 }
@@ -37,7 +37,6 @@ int palindrome(listint_t *head, int n)
 */
 int is_palindrome(listint_t **head)
 {
-	int n = 0;
 	listint_t *aux;
 
 	if (head == NULL || *head == NULL)
@@ -46,10 +45,7 @@ int is_palindrome(listint_t **head)
 	aux = *head;
 
 	while (aux->next != NULL)
-	{
-		n++;
 		aux = aux->next;
-	}
 
-	return (palindrome(*head, n));
+	return (palindrome(*head, aux));
 }
