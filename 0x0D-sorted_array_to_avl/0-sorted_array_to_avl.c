ed2b847cef416ee914368f3838b07df1f648231a
Carlos <926@holbertonschool.com>
2020-08-05 20:19:18 -0500

    _

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,5 +1,35 @@
 #include "binary_trees.h"
 /**
+ * array_to_avl - extra function
+ * @array: pointer to the array
+ * @start: starting index
+ * @end: ending index
+ * @parent: pointer to parent node
+ * Return: new node
+ */
+avl_t *array_to_avl(int *array, int start, int end, avl_t *parent)
+{
+	avl_t *new;
+	int mid;
+
+	if (start > end)
+		return (NULL);
+
+	mid = (start + end) / 2;
+	new = malloc(sizeof(avl_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = array[mid];
+	new->parent = parent;
+	new->left = array_to_avl(array, start, mid - 1, new);
+	new->right = array_to_avl(array, mid + 1, end, new);
+
+	return (new);
+}
+
+/**
  * sorted_array_to_avl - inserts sorted array elements to AVL tree
  * @array: pointer to sorted array
  * @size: number of elementss of array
@@ -7,26 +37,9 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	size_t mid;
-	avl_t *root;
 
-	if (array == NULL || size < 1)
+	if (array == NULL)
 		return (NULL);
 
-	root = malloc(sizeof(avl_t));
-	if (root == NULL)
-		return (NULL);
-
-	mid = size / 2;
-
-	root->n = array[mid];
-	root->left = sorted_array_to_avl(array, mid);
-	root->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
-
-	if (root->left != NULL)
-		root->left->parent = root;
-	if (root->right)
-		root->right->parent = root;
-
-	return (root);
+	return (array_to_avl(array, 0, size - 1, NULL));
 }
