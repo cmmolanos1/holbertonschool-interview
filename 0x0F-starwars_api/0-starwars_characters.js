b95ce19d5b63bf329c14589b2757af19d4ae99fe
Carlos <926@holbertonschool.com>
2020-08-19 12:11:00 -0500

    [Upload] Task0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,13 +2,16 @@
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 
-request(url, function (error, response, body) {
+request(url, async function (error, response, body) {
   if (error) throw error;
   const characters = JSON.parse(body).characters;
   for (const c in characters) {
-    request(characters[c], function (error, response, body) {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
+    const res = await new Promise((resolve, reject) => {
+      request(characters[c], function (error, response, body) {
+        if (error) throw error;
+        resolve(JSON.parse(body).nakme);
+      });
     });
+    console.log(res);
   }
 });
