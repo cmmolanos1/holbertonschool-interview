817fea6c3e709358cfd373bf2c42f0a2e2294353
Carlos <926@holbertonschool.com>
2020-09-28 10:24:57 -0500

    Upload task

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef INV_HOLBERTON_H
+#define INV_HOLBERTON_H
+
+int _putchar(char c);
+
+void Error(void);
+int allDigits(char **arg);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
