b2bb88e40de22b3cb8dba123b6a2f9ac76ab0eea
Carlos <926@holbertonschool.com>
2020-12-17 10:06:28 -0500

    [Upload] Task0

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """Prints the n-pascal triangle"""
+    lista = []
+    if n <= 0:
+        return lista
+    for i in range(0, n):
+        lista.append([1] * (i + 1))
+        if i >= 2:
+            for j in range(1, len(lista[i]) - 1):
+                lista[i][j] = lista[i - 1][j] + lista[i - 1][j - 1]
+    return lista
